63c4924a1499c93258fbb3d70fee17dc294f00a8
Polina Maximova <polinkamax0903@gmail.com>
2026-02-09 22:38:45 +0400

    Выполнение задание 4

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
 public class Main{
     public static void main (String[] args) {
-        BankAccount account = new BankAccount("Vladik");
-        System.out.println(account.name);
+        BankAccount account1 = new BankAccount("Vladik");
+        System.out.println(account1.name);
+        BankAccount account2 = new BankAccount("Dimasik");
+        System.out.println(account2.name);
+
+        System.out.println("Создание счета");
+        System.out.println(account1);
+        System.out.println(account2);
+
+        System.out.println("Пополнение счета на 500 " + account1.Deposit(500));
+        System.out.println("Cнятие 100 рублей " + account1.Withdraw(100));
+        System.out.println("Перевод 50 рублей Dimasikу  " + account1.Transfer(account2, 50));
     }
 }
