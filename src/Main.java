073f86ef59c3f3334fde9b3bee07fcb83b3fd825
Polina Maximova <polinkamax0903@gmail.com>
2026-02-16 21:38:42 +0400

    Выполнение задание 1 пункт 3

--- src/Main.java
+++ src/Main.java
@@ -7,13 +7,14 @@ public class Main{
         for (int i=0; i<50; i++){
             car[i]= 2000 + random.nextInt(26);
         }
-        System.out.println("Авто выпущены после 2015");
-            int auto2015 = 0;
-            for (int i=0; i<50; i++){
-                if(car[i]>2015){
-                    System.out.print(car[i] + " ");
-                    auto2015++;
-                }
-            }
+        System.out.println("Средний возраст авто");
+        int Sum = 0;
+        int CurrentYear = 2026;
+        for (int i = 0; i <50; i++){
+            int age = CurrentYear - car[i];
+            Sum = Sum +age;
         }
+        double AverageAge = (double) Sum/50;
+        System.out.print(AverageAge);
+    }
 }
