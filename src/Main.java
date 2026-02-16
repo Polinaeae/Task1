5da9fe45a0725e39636f7c83dfd15ecade5fbfa8
Polina Maximova <polinkamax0903@gmail.com>
2026-02-16 21:20:10 +0400

    Выполнение задание 1 пункт 1

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,15 @@
+import java.util.Random;
+
 public class Main{
     public static void main (String[] args) {
-        BankAccount account1 = new BankAccount("Vladik");
-        System.out.println(account1.name);
-        BankAccount account2 = new BankAccount("Dimasik");
-        System.out.println(account2.name);
-
-        System.out.println("Создание счета");
-        System.out.println(account1);
-        System.out.println(account2);
-
-        System.out.println("Пополнение счета на 500 " + account1.Deposit(500));
-        System.out.println("Cнятие 100 рублей " + account1.Withdraw(100));
-        System.out.println("Перевод 50 рублей Dimasikу  " + account1.Transfer(account2, 50));
+        int[] car = new int[50];
+        Random random = new Random();
+        for (int i=0; i<50; i++){
+            car[i]= 2000 + random.nextInt(26);
+        }
+        System.out.println("Годы выпуска авто");
+        for (int i =0; i<50; i++){
+            System.out.println(car[i] + " ");
+        }
     }
 }
